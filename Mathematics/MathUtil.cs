0c025b5ce923bd9462f667cb1a23e04bda759058
RealShiftless <aaronmoulding@live.nl>
2025-05-10 14:52:05 +0200

    Dungeon Builder Changes

---
+++ Mathematics/MathUtil.cs
@@ -0,0 +1,13 @@
+﻿using Shiftless.Clockwork.Retro.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoRogue.Mathematics
+{
+    public static class MathUtil
+    {
+    }
+}
--- World/Building/DungeonBuilder.cs
+++ World/Building/DungeonBuilder.cs
@@ -88,23 +88,55 @@ namespace DemoRogue.World.Building
             // And we set the actual room
             _chunks[gridX, gridY].SetRoom(body, type);
         }
-        public void CreateRoom(Point8 gridPosition, Rect8 localBody, RoomTypes type) => SetRoom(gridPosition.X, gridPosition.Y, localBody, type);
+        public void SetRoom(Point8 gridPosition, Rect8 localBody, RoomTypes type) => SetRoom(gridPosition.X, gridPosition.Y, localBody, type);
+
+        private void CreatePath(ChunkBuilder sourceChunk, Point8 sourcePos, ChunkBuilder destChunk, Point8 destPos, Direction direction, int turnIndex)
+        {
+            // We create and add the path
+            Path path = new(sourcePos, destPos, direction.ToAxis(), turnIndex);
+            byte pathId = (byte)_paths.Count;
+
+            _paths.Add(path);
+
+            // And now we go over each chunk in this direction and add it to the ting until we get to the dest pos
+            // Because if we do this, all rooms in between are null. This means we don't really have to set if the chunk leads to source
+            Point8 curGridPos = sourceChunk.GridPosition;
+            while ((curGridPos = curGridPos.Translate(direction, 1)) != destChunk.GridPosition)
+                _chunks[curGridPos.X, curGridPos.Y].AddPath(pathId);
+
+            // Finally we add the paths to the source and dest
+            sourceChunk.AddPath(pathId);
+            destChunk.AddPath(pathId, sourceChunk.LeadsToSource);
+        }
+
+        public void CreatePath(Point8 sourcePos, Point8 destPos, Direction direction, int turnIndex)
+        {
+            // We get the grid positions of the start and origin
+            Point8 sourceGridPos = sourcePos / (GridWidth, GridHeight);
+            Point8 destGridPos = destPos / (GridWidth, GridHeight);
+
+            // We get the actual chunks
+            ChunkBuilder sourceChunk = _chunks[sourceGridPos.X, sourceGridPos.Y];
+            ChunkBuilder destChunk = _chunks[destGridPos.X, destGridPos.Y];
+
+            // And we add the path using the func
+            CreatePath(sourceChunk, sourcePos, destChunk, destPos, direction, turnIndex);
+        }
 
         public bool CreatePath(Point8 sourceGridPos, Direction direction)
         {
             // Now we get the actual rooms
-            ChunkBuilder source = _chunks[sourceGridPos.X, sourceGridPos.Y];
-            ChunkBuilder? dest = GetNextChunkWithRoom(sourceGridPos, direction);
+            ChunkBuilder sourceChunk = _chunks[sourceGridPos.X, sourceGridPos.Y];
+            ChunkBuilder? destChunk = GetNextChunkWithRoom(sourceGridPos, direction);
 
             // If there was no valid destination we return false
-            if (dest == null)
+            if (destChunk == null)
                 return false;
 
-            // Get the axis
             Axis axis = direction.ToAxis();
 
             // Get the distance using said axis
-            int dist = source.RoomBody.DistanceFrom(dest.RoomBody, axis);
+            int dist = sourceChunk.RoomBody.DistanceFrom(destChunk.RoomBody, axis);
 
             // For now I will throw an error when rooms are touching, because they currently shouldn't. Later I will add merged rooms, then i need to implement behaviors for that.
             if (dist < 2)
@@ -112,31 +144,18 @@ namespace DemoRogue.World.Building
 
             // Now we create the actual path source and destination positions
             // TODO: Add path overlap func, so like regenerate a path if it like really overlaps
-            Point8 sourcePos = source.GeneratePathPoint(direction);
-            Point8 destPos = dest.GeneratePathPoint(direction.Invert());
+            Point8 sourcePos = sourceChunk.GeneratePathPoint(direction);
+            Point8 destPos = destChunk.GeneratePathPoint(direction.Invert());
 
             // And using the distance between the two rooms we generate a turn point
             int turnIndex = RNG.Next(dist - 2) + 1;
 
-            // We create and add the path
-            Path path = new(sourcePos, destPos, axis, turnIndex);
-            byte pathId = (byte)_paths.Count;
-
-            _paths.Add(path);
-
-            // And now we go over each chunk in this direction and add it to the ting until we get to the dest pos
-            // Because if we do this, all rooms in between are null. This means we don't really have to set if the chunk leads to source
-            Point8 gridPos = sourceGridPos;
-            while ((gridPos = gridPos.Translate(direction, 1)) != dest.GridPosition)
-                _chunks[gridPos.X, gridPos.Y].AddPath(pathId);
-
-            // Finally we add the paths to the source and dest
-            source.AddPath(pathId);
-            dest.AddPath(pathId, source.LeadsToSource);
+            CreatePath(sourceChunk, sourcePos, destChunk, destPos, direction, turnIndex);
 
             // And we return true because we succesfully created the path :)))
             return true;
         }
+        public bool CreatePath(int gridX, int gridY, Direction direction) => CreatePath(new(gridX, gridY), direction);
 
         private ChunkBuilder? GetNextChunkWithRoom(Point8 gridPos, Direction direction)
         {
@@ -155,10 +174,6 @@ namespace DemoRogue.World.Building
 
             // And now we just return
             return _chunks[gridPos.X, gridPos.Y];
-        }
-
-        public void CreatePath(int gridX, int gridY, Direction direction)
-        {
         }
 
         public void EnumerateGrid(GridEnumerationEventHandler action)
